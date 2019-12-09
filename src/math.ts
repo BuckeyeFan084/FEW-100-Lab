f0eaca6581000d7aceeec93cd7b607d4cd17b8a8
BuckeyeFan084 <56597617+BuckeyeFan084@users.noreply.github.com>
2019-12-09 14:24:30 +0000

    12/9/2019

--- src/math.ts
+++ src/math.ts
@@ -1,15 +1,20 @@
-export function tipAmount(a: number) {
-    return a * .10;
+export function totalBill(number1: number) {
+    return number1 * .10;
 }
+
+
+
+
 
 
 const number1El = document.getElementById('number1') as HTMLInputElement;
 const number1E2 = document.getElementById('number2') as HTMLInputElement;
-const multiplyButton = document.getElementById('tipAmount') as HTMLInputElement;
+const multiplyButton1 = document.getElementById('tipAmount') as HTMLInputElement;
+
 const answerEl = document.getElementById('answer') as HTMLSpanElement;
 
 // tslint:disable-next-line: only-arrow-functions
-multiplyButton.addEventListener('click', function() {
+multiplyButton1.addEventListener('click', function () {
 
     const n1 = number1El.valueAsNumber;
     const n2 = number1E2.valueAsNumber;
